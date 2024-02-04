a5ee2561a999f5c2e4f030ac96415575b9f2eb72
Alexander <post@alexanderfridriksson.com>
2024-02-04 11:12:31 +0000

    updating format time

--- bench_surreal_deal/scripts/bench_utils.py
+++ bench_surreal_deal/scripts/bench_utils.py
@@ -66,7 +66,7 @@ def get_gen_uuid4_unique_list(total_num, list_num, seed=42):
     return uuid_list
 
 
-def format_time(raw_time, unit="ms", precision=0):
+def format_time(raw_time, unit="ms", precision=0, unit_label=True):
     """
     Takes in time in nanoseconds
     Returns formatted time in selected unit
@@ -78,12 +78,20 @@ def format_time(raw_time, unit="ms", precision=0):
 
     converted_time = raw_time / selected_unit
 
-    if int(converted_time) == converted_time:
-        formatted_time = f"{converted_time} {unit}"
-    elif precision==0:
-        formatted_time = f"{int(round(converted_time, precision))} {unit}"
-    else:
-        formatted_time = f"{round(converted_time, precision)} {unit}"
+    if unit_label == True:
+        if int(converted_time) == converted_time:
+            formatted_time = f"{converted_time} {unit}"
+        elif precision==0:
+            formatted_time = f"{int(round(converted_time, precision))} {unit}"
+        else:
+            formatted_time = f"{round(converted_time, precision)} {unit}"
+    if unit_label == False:
+        if int(converted_time) == converted_time:
+            formatted_time = converted_time
+        elif precision==0:
+            formatted_time = int(round(converted_time, precision))
+        else:
+            formatted_time = round(converted_time, precision)
 
     return formatted_time
 
