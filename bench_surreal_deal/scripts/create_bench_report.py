78ca935cabddfc1048f88bb50b25801ba4300610
Alexander <post@alexanderfridriksson.com>
2024-02-11 10:51:50 +0000

    add metrics tables

--- bench_surreal_deal/scripts/create_bench_report.py
+++ bench_surreal_deal/scripts/create_bench_report.py
@@ -1,7 +1,7 @@
 import pathlib
 import json
 
-from bench_utils import calculate_latency_metrics, plot_box, create_markdown_table
+from bench_utils import create_markdown_metrics_table
 
 # create report
 
@@ -15,16 +15,12 @@ from bench_utils import calculate_latency_metrics, plot_box, create_markdown_tab
 output_files_path = pathlib.Path(__file__).parents[1] / "output_files"
 
 # Get SurrealDB results
-# with open(output_files_path / pathlib.Path("surrealdb_bench_output.json"), "r") as file:
-#     surrealdb_bench_results = json.read(file)
+with open(output_files_path / pathlib.Path("surrealdb_bench_output.json"), "r") as file:
+    sdb_results = json.load(file)
 
 # Get MongoDB results
 with open(output_files_path / pathlib.Path("mongodb_bench_output.json"), "r") as file:
-    mongodb_bench_results = json.load(file)
-
-
-sdb_test = calculate_latency_metrics(mongodb_bench_results["total_write_duration"])
-mdb_test = calculate_latency_metrics(mongodb_bench_results["total_read_duration"])
+    mdb_results = json.load(file)
 
 
 report = f"""
@@ -40,20 +36,201 @@ P99 latency (ms)
 P99 read latency (ms)
 P99 write latency (ms) 
 
+
+# overall latency
+
+{create_markdown_metrics_table(
+    sdb_results["total_time_duration"], 
+    mdb_results["total_time_duration"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["total_read_duration"], 
+    mdb_results["total_read_duration"]
+)}
+
+
+sdb_results["total_write_duration"],
+mdb_results["total_write_duration"]
+
+sdb_results["total_time_duration"],
+mdb_results["total_time_duration"]
+
+sdb_results["total_queries_count"],
+mdb_results["total_queries_count"]
+
+sdb_results["total_throughput_qps"],
+mdb_results["total_throughput_qps"]
+
+sdb_results["wall_time_duration"],
+mdb_results["wall_time_duration"]
+
+sdb_results["index_duration"],
+mdb_results["index_duration"]
+
+## Grouped results
+
+### Read latency
+
+{create_markdown_metrics_table(
+    sdb_results["read_filter_duration"],
+    mdb_results["read_filter_duration"]
+)}
+
+
+{create_markdown_metrics_table(
+    sdb_results["read_relationships_duration"],
+    mdb_results["read_relationships_duration"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["read_aggregation_duration"],
+    mdb_results["read_aggregation_duration"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["update_duration"],
+    mdb_results["update_duration"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["delete_duration"],
+    mdb_results["delete_duration"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["transactions_duration"],
+    mdb_results["transactions_duration"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["insert_duration"],
+    mdb_results["insert_duration"]
+)}
+
 ## Detailed results
 
 ### Insert
-both throughput and latency?
 
-**Shape of the data:** box plot on an independent scale, showing the below info visually
+{create_markdown_metrics_table(
+    sdb_results["insert_person"],
+    mdb_results["insert_person"]
+)}
 
-SDB: {plot_box(mongodb_bench_results["total_write_duration"])}
-MDB: {plot_box(mongodb_bench_results["total_read_duration"])}
+{create_markdown_metrics_table(
+    sdb_results["insert_artist"],
+    mdb_results["insert_artist"]
+)}
 
-**Metrics table:**
+{create_markdown_metrics_table(
+    sdb_results["insert_order"],
+    mdb_results["insert_order"]
+)}
 
-{create_markdown_table(sdb_test, mdb_test)}
+{create_markdown_metrics_table(
+    sdb_results["insert_review"],
+    mdb_results["insert_review"]
+)}
 
+{create_markdown_metrics_table(
+    sdb_results["q4_index"],
+    mdb_results["q4_index"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["q5_index"],
+    mdb_results["q5_index"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["q8_index"],
+    mdb_results["q8_index"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["q10_index"],
+    mdb_results["q10_index"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["q1"],
+    mdb_results["q1"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["q2"],
+    mdb_results["q2"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["q3"],
+    mdb_results["q3"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["q4"],
+    mdb_results["q4"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["q5"],
+    mdb_results["q5"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["q6"],
+    mdb_results["q6"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["update_one"],
+    mdb_results["update_one"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["update_many"],
+    mdb_results["update_many"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["delete_one"],
+    mdb_results["delete_one"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["delete_many"],
+    mdb_results["delete_many"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["tx1_insert_update"],
+    mdb_results["tx1_insert_update"]
+)}
+
+{create_markdown_metrics_table(
+    sdb_results["tx2_insert"],
+    mdb_results["tx2_insert"]
+)}
 
 """
 print(report)
+
+
+# sdb_results["delete_query_count"],
+# mdb_results["delete_query_count"]
+
+# sdb_results["update_query_count"],
+# mdb_results["update_query_count"]
+
+# sdb_results["index_query_count"],
+# mdb_results["index_query_count"]
+
+# sdb_results["insert_query_count"],
+# mdb_results["insert_query_count"]
+
+# sdb_results["read_query_count"],
+# mdb_results["read_query_count"]
+
+# sdb_results["transactions_count"],
+# mdb_results["transactions_count"]
+
