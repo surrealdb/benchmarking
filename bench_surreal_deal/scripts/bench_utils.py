874668889487fe07d8af529d4d38ecbc17f86d76
Alexander <post@alexanderfridriksson.com>
2024-02-12 01:34:59 +0000

    adding interpolation and setting it to default

--- bench_surreal_deal/scripts/bench_utils.py
+++ bench_surreal_deal/scripts/bench_utils.py
@@ -104,50 +104,80 @@ def format_time_list(raw_time_list, unit="ms", precision=0, unit_label=False):
     return [format_time(raw_time, unit=unit, precision=precision, unit_label=unit_label) for raw_time in raw_time_list]
 
 
-def calculate_latency_percentile(result_list, unit="ms"):
+def calculate_latency_percentile(result_list, unit="ms", interpolation=True):
     """
     Takes in a list of integers and calculates percentile metrics
     """
-    sorted_list = sorted(result_list)
-    number_of_results = len(result_list)-1 # accounting for zero-based indexing
-    
-    # // same as using math floor
-    percentile_metrics = {
-        f"min({unit})": format_time(min(sorted_list), unit=unit),
-        f"p1({unit})": format_time(sorted_list[(number_of_results * 1) // 100], unit=unit),
-        f"p5({unit})": format_time(sorted_list[(number_of_results * 5) // 100], unit=unit),
-        f"p25({unit})": format_time(sorted_list[(number_of_results * 25) // 100], unit=unit),
-        f"p50({unit})": format_time(sorted_list[(number_of_results * 50) // 100], unit=unit),
-        f"p75({unit})": format_time(sorted_list[(number_of_results * 75) // 100], unit=unit),
-        f"p90({unit})": format_time(sorted_list[(number_of_results * 90) // 100], unit=unit),
-        f"p99({unit})": format_time(sorted_list[(number_of_results * 99) // 100], unit=unit),
-        f"max({unit})": format_time(max(sorted_list), unit=unit)
-    }
+
+    if interpolation == False:
+        sorted_list = sorted(result_list)
+        number_of_results = len(result_list)-1 # accounting for zero-based indexing
+
+        # // same as using math floor
+        percentile_metrics = {
+            f"min({unit})": format_time(min(sorted_list), unit=unit),
+            f"p1({unit})": format_time(sorted_list[(number_of_results * 1) // 100], unit=unit),
+            f"p5({unit})": format_time(sorted_list[(number_of_results * 5) // 100], unit=unit),
+            f"p25({unit})": format_time(sorted_list[(number_of_results * 25) // 100], unit=unit),
+            f"p50({unit})": format_time(sorted_list[(number_of_results * 50) // 100], unit=unit),
+            f"p75({unit})": format_time(sorted_list[(number_of_results * 75) // 100], unit=unit),
+            f"p90({unit})": format_time(sorted_list[(number_of_results * 90) // 100], unit=unit),
+            f"p99({unit})": format_time(sorted_list[(number_of_results * 99) // 100], unit=unit),
+            f"max({unit})": format_time(max(sorted_list), unit=unit)
+        }
+
+    if interpolation == True:
+        percentile_metrics = {
+            f"min({unit})": format_time(min(result_list), unit=unit),
+            f"p1({unit})": format_time(int(np.percentile(result_list, 1)), unit=unit),
+            f"p5({unit})": format_time(int(np.percentile(result_list, 5)), unit=unit),
+            f"p25({unit})": format_time(int(np.percentile(result_list, 25)), unit=unit),
+            f"p50({unit})": format_time(int(np.percentile(result_list, 50)), unit=unit),
+            f"p75({unit})": format_time(int(np.percentile(result_list, 75)), unit=unit),
+            f"p90({unit})": format_time(int(np.percentile(result_list, 90)), unit=unit),
+            f"p99({unit})": format_time(int(np.percentile(result_list, 99)), unit=unit),
+            f"max({unit})": format_time(max(result_list), unit=unit)
+        }
     return percentile_metrics
 
 def throughput_calc(query_count, duration, unit="s", precision=1):
     throughput = query_count / format_time(duration, unit=unit, precision=precision)
     return round(throughput, precision)
 
-def calculate_throughput_percentile(result_list, unit="s"):
+def calculate_throughput_percentile(result_list, unit="s", interpolation=True):
     """
     Takes in a list of numbers and calculates percentile metrics
     """
-    sorted_list = sorted(result_list)
-    number_of_results = len(result_list)-1 # accounting for zero-based indexing
-    
-    # // same as using math floor
-    percentile_metrics = {
-        f"min({unit})": min(sorted_list),
-        f"p1({unit})": sorted_list[(number_of_results * 1) // 100],
-        f"p5({unit})": sorted_list[(number_of_results * 5) // 100],
-        f"p25({unit})": sorted_list[(number_of_results * 25) // 100],
-        f"p50({unit})": sorted_list[(number_of_results * 50) // 100],
-        f"p75({unit})": sorted_list[(number_of_results * 75) // 100],
-        f"p90({unit})": sorted_list[(number_of_results * 90) // 100],
-        f"p99({unit})": sorted_list[(number_of_results * 99) // 100],
-        f"max({unit})": max(sorted_list)
-    }
+    if interpolation == False:
+        sorted_list = sorted(result_list)
+        number_of_results = len(result_list)-1 # accounting for zero-based indexing
+
+        # // same as using math floor
+        percentile_metrics = {
+            f"min({unit})": min(sorted_list),
+            f"p1({unit})": sorted_list[(number_of_results * 1) // 100],
+            f"p5({unit})": sorted_list[(number_of_results * 5) // 100],
+            f"p25({unit})": sorted_list[(number_of_results * 25) // 100],
+            f"p50({unit})": sorted_list[(number_of_results * 50) // 100],
+            f"p75({unit})": sorted_list[(number_of_results * 75) // 100],
+            f"p90({unit})": sorted_list[(number_of_results * 90) // 100],
+            f"p99({unit})": sorted_list[(number_of_results * 99) // 100],
+            f"max({unit})": max(sorted_list)
+        }
+
+    if interpolation == True:
+        percentile_metrics = {
+            f"min({unit})": min(result_list),
+            f"p1({unit})": np.percentile(result_list, 1),
+            f"p5({unit})": np.percentile(result_list, 5),
+            f"p25({unit})": np.percentile(result_list, 25),
+            f"p50({unit})": np.percentile(result_list, 50),
+            f"p75({unit})": np.percentile(result_list, 75),
+            f"p90({unit})": np.percentile(result_list, 90),
+            f"p99({unit})": np.percentile(result_list, 99),
+            f"max({unit})": max(result_list)
+        }
+
     return percentile_metrics
 
 def create_markdown_metrics_table(SDB_result, MDB_result, unit="ms", metric="latency"):
